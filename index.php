8fe2d1e52b05a5657d2ceb0367fd016635808345
ROSA GENETTI <rgenetti@lesley.edu>
2019-10-02 20:26:42 -0400

    added php for blog post titles

--- index.php
+++ index.php
@@ -2,9 +2,23 @@
 ?>
 <html>
 <body>
+    <?php
+        function getPostTitlesFromDatabase () {
+            //todo in module 4
+            //get this data from datbase instead of hardcoding it
+            $postTitles = array ("Blog Post 1", "Blog Post 2", "Blog Post 3");
+            return $postTitles;
+        }
+        ?>
            <main>
                 <ul>
-                    <li><a href= "Post.php">Blog Post 1</a></li>
+                    <?php
+                    $postTitles = getPostTitlesFromDatabase ();
+
+                    foreach ($postTitles as $postTitle){
+                        echo "<li><a href='post.php?title='. $postTitle . "'>" . $postTitle . "</a></li>";
+                    }
+                    ?>
                  </ul>
            </main>
     </body>
